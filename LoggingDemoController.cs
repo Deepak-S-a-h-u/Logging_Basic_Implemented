037a1ab8a6af1d0e6f4724003061a48b253995f9
Deepak_Sahu <programmer.zone101@gmail.com>
2022-12-21 18:29:16 +0530

    understand Logging By this Web application's Implimentation

---
+++ LoggingDemoController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Learning_Logging
+{
+    public class LoggingDemoController:Controller
+    {
+        private readonly ILogger<LoggingDemoController> _logger;
+        public LoggingDemoController(ILogger<LoggingDemoController> logger)
+        {
+            _logger = logger;       
+        }
+
+        public void sampleMethod()
+        {
+            _logger.LogTrace("");
+            _logger.LogTrace("");
+            _logger.LogInformation("");
+            _logger.LogCritical("");
+            _logger.LogDebug("");
+            _logger.LogError("");
+            _logger.LogWarning("");
+        }
+    }
+}
